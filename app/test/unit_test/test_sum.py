3d6b7bb7962fa6e59ec673dacc027b35fcbe6221
Osuke Uesugi <ousuke.uesugi@bebit.com>
2020-06-04 16:34:21 +0900

    test faild

--- app/test/unit_test/test_sum.py
+++ app/test/unit_test/test_sum.py
@@ -5,7 +5,7 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         arg_a = 1
         arg_b = 2
-        expect = arg_a + arg_b
+        expect = arg_a + arg_b + 1
         actual = sum(arg_a, arg_b)
         self.assertEqual(actual, expect)
 
