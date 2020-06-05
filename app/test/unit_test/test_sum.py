f5c2ecc3bb5b66709bd78bc2e3499f4d33eaec4d
Osuke Uesugi <ousuke.uesugi@bebit.com>
2020-06-05 09:14:08 +0900

    Fix unit test

--- app/test/unit_test/test_sum.py
+++ app/test/unit_test/test_sum.py
@@ -5,7 +5,7 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         arg_a = 1
         arg_b = 2
-        expect = arg_a + arg_b + 1
+        expect = arg_a + arg_b
         actual = sum(arg_a, arg_b)
         self.assertEqual(actual, expect)
 
